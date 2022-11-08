4caa0b6c0883d00f574cca80396a57471e93d3b2
Daniel Butler <dannyeb@gmail.com>
2022-11-08 12:21:50 +0000

    feat: initial release

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -3,18 +3,31 @@
 ##############################################################################
 
 module "resource_group" {
-  source = "git::https://github.com/terraform-ibm-modules/terraform-ibm-resource-group.git?ref=v1.0.1"
+  source = "git::https://github.com/terraform-ibm-modules/terraform-ibm-resource-group.git?ref=v1.0.2"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
 }
 
 ##############################################################################
-# VPC
+# Key Protect module
 ##############################################################################
 
-resource "ibm_is_vpc" "vpc" {
-  name           = "${var.prefix}-vpc"
-  resource_group = module.resource_group.resource_group_id
-  tags           = var.resource_tags
+module "key_protect_module" {
+  source            = "git::https://github.com/terraform-ibm-modules/terraform-ibm-key-protect.git?ref=v1.0.0"
+  key_protect_name  = "${var.prefix}-kp"
+  resource_group_id = module.resource_group.resource_group_id
+  region            = var.region
+  tags              = var.resource_tags
+}
+
+##############################################################################
+# Key Ring module
+##############################################################################
+
+module "key_protect_key_ring" {
+  source        = "../.."
+  instance_id   = module.key_protect_module.key_protect_guid
+  key_ring_id   = "${var.prefix}-key-ring"
+  endpoint_type = "public"
 }
--- examples/default/outputs.tf
+++ examples/default/outputs.tf
@@ -2,11 +2,6 @@
 # Outputs
 ##############################################################################
 
-output "vpc_id" {
-  description = "ID of VPC created"
-  value       = ibm_is_vpc.vpc.id
-}
-
 output "resource_group_name" {
   description = "Resource group name"
   value       = module.resource_group.resource_group_name
@@ -15,4 +10,14 @@ output "resource_group_name" {
 output "resource_group_id" {
   description = "Resource group ID"
   value       = module.resource_group.resource_group_id
+}
+
+output "key_protect_id" {
+  description = "Key Protect Instance ID"
+  value       = module.key_protect_module.key_protect_guid
+}
+
+output "key_protect_key_ring_id" {
+  description = "Key Protect Key Ring ID"
+  value       = module.key_protect_key_ring.key_ring_id
 }
