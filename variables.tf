c3b8ecf747f6eae44a3e52aa99d45f38979240a7
Jordan-Williams2 <Jordan.Williams2@ibm.com>
2023-10-03 13:35:10 +0100

    fix: SKIP UPGRADE TEST

--- variables.tf
+++ variables.tf
@@ -19,7 +19,7 @@ variable "instance_id" {
 
 variable "force_delete" {
   type        = bool
-  description = "Set to `True` if you wish to force delete the kms `key_ring`."
+  description = "Set to `True` if you wish to force delete the kms `key_ring`"
   default     = true
 }
 
