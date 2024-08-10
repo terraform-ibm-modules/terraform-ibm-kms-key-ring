4564ff7359e9c0c1f06e027c1b722e61a659b65b
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-08-10 06:39:50 +0100

    chore(deps): update terraform terraform-ibm-modules/key-protect/ibm to v2.8.2 (#612)

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "key_protect" {
   source            = "terraform-ibm-modules/key-protect/ibm"
-  version           = "2.8.1"
+  version           = "2.8.2"
   key_protect_name  = "${var.prefix}-kp"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
