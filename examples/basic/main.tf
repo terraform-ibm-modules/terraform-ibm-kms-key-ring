7331a0a97bc6af4c649d9308cd8df672f74243ff
Matthew Lemmond <Matthew.Lemmond@ibm.com>
2024-10-04 13:47:58 -0400

    feat: remove the `force_delete` variable as this feature has been deprecated (#632)

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -3,8 +3,7 @@
 ##############################################################################
 
 module "kms_key_ring" {
-  source       = "../.."
-  instance_id  = var.existing_kms_instance_guid
-  key_ring_id  = "${var.prefix}-key-ring"
-  force_delete = true # Setting it to true for testing purpose
+  source      = "../.."
+  instance_id = var.existing_kms_instance_guid
+  key_ring_id = "${var.prefix}-key-ring"
 }
