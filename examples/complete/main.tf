402603b57be3c4d227393c7e11b17c476109591c
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-06-21 06:40:05 +0000

    chore(deps): update terraform dependencies

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -4,7 +4,7 @@
 
 module "resource_group" {
   source  = "terraform-ibm-modules/resource-group/ibm"
-  version = "1.2.0"
+  version = "1.2.1"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "key_protect" {
   source            = "terraform-ibm-modules/key-protect/ibm"
-  version           = "2.10.2"
+  version           = "2.10.3"
   key_protect_name  = "${var.prefix}-kp"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
