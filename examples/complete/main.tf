70a537e4779d8fc9a8c6d4cc18c42138d60e172c
Valerio Bontempi <valerio.bontempi@it.ibm.com>
2026-08-06 15:20:03 +0200

    fix: fixed renovate deps

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -20,7 +20,7 @@ module "key_protect" {
   key_protect_name  = "${var.prefix}-kp"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
-  tags              = var.resource_tags
+  resource_tags     = var.resource_tags
 }
 
 ##############################################################################
